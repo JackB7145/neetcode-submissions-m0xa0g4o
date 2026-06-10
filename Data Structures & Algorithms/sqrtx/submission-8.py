0c495d3367bf97a59423eb41669f75adea650aa3
Jack Branston <jbranston6@gmail.com>
2026-06-09 22:40:48 -0400

    Add: sqrtx - submission-8

---
+++ Data Structures & Algorithms/sqrtx/submission-8.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+
+        while l <= r:
+            m = (l+r)//2
+            if m ** 2 == x:
+                return m
+
+            elif m ** 2 > x:
+                r = m - 1
+            
+            else:
+                val = m
+                l = m + 1
+
+        return val
