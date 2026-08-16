20b31d7751b8c34327cadf4685780766f61ed64a
Jack Branston <jbranston6@gmail.com>
2026-08-16 02:05:47 -0400

    Add: partition-labels - submission-4

---
+++ Data Structures & Algorithms/partition-labels/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        cnt = Counter(s)
+
+        currLength = 0
+        res = []
+        loadedCount = {} 
+
+        for c in s:
+            currLength += 1 #0
+            if c in loadedCount:
+                loadedCount[c] -= 1
+                if not loadedCount[c]:
+                    del loadedCount[c]
+
+            else:
+                if cnt[c] - 1 != 0:
+                    loadedCount[c] = cnt[c]-1
+
+            if len(loadedCount) == 0:
+                res.append(currLength)
+                currLength = 0
+        
+        return res
+
+            
